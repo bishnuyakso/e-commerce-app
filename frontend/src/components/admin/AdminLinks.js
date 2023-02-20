8fea624c5cfc10b801a8f09cc200a2b95b7df1d8
bishnuyakso <yaksobishnu@gmail.com>
2023-02-20 21:57:04 +0800

    updated frontend backend

--- frontend/src/components/admin/AdminLinks.js
+++ frontend/src/components/admin/AdminLinks.js
@@ -1,28 +1,31 @@
 import { Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { logout } from "../../redux/actions/userActions";
+import { useDispatch } from "react-redux";
+
 const AdminLinks = () => {
+  const dispatch = useDispatch();
   return (
     <Navbar bg="light" variant="light">
-        <Nav className="flex-column">
-      <LinkContainer to="/admin/orders">
-        <Nav.Link>Orders</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/admin/products">
-        <Nav.Link>Products</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/admin/users">
-        <Nav.Link>Users</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/admin/chats">
-        <Nav.Link>Chats</Nav.Link>
-      </LinkContainer>
-      <LinkContainer to="/admin/analytics">
-        <Nav.Link>Analytics</Nav.Link>
-      </LinkContainer>
-      <Nav.Link>Logout</Nav.Link>
-    </Nav>
+      <Nav className="flex-column">
+        <LinkContainer to="/admin/orders">
+          <Nav.Link>Orders</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/admin/products">
+          <Nav.Link>Products</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/admin/users">
+          <Nav.Link>Users</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/admin/chats">
+          <Nav.Link>Chats</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/admin/analytics">
+          <Nav.Link>Analytics</Nav.Link>
+        </LinkContainer>
+        <Nav.Link onClick={() => dispatch(logout())}>Logout</Nav.Link>
+      </Nav>
     </Navbar>
-    
   );
 };
 
